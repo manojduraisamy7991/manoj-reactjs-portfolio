2ec3777c7cbf747cf37eed2880e9eadf27a77c18
fastbokz <fastbokz@gmail.com>
2024-03-20 22:20:42 +0530

    skills updates

--- src/app/components/TechStack.tsx
+++ src/app/components/TechStack.tsx
@@ -1,20 +1,12 @@
-  
-  // For icons https://icons8.com/icons/set/css
-     const techStack = [
-      { name: 'JavaScript', image: 'https://img.icons8.com/?size=48&id=PXTY4q2Sq2lG&format=png'},
-        { name: 'Html', image: 'https://img.icons8.com/?size=48&id=20909&format=png' },
-        { name: 'Css', image: 'https://img.icons8.com/?size=60&id=3BTBsJs5myRy&format=png'},
-        { name: 'React.js', image: 'https://img.icons8.com/?size=80&id=wPohyHO_qO1a&format=png'}
-      ];
+  import {Skills} from '../constants/Skills'
     
   export const TechStack = () => {
- 
     return (
       <section className="bg-gray-100 py-16">
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold text-center mb-8 text-center">Tech Stack</h2>
           <div className="flex flex-wrap justify-center">
-            {techStack.map((tech, index) => (
+            {Skills.map((tech, index) => (
               <div key={index} className="w-1/2 sm:w-1/4 md:w-1/6 p-4">
                 <img src={tech.image} alt={tech.name} className="mx-auto h-12" />
                 <p className="text-center mt-4">{tech.name}</p>
---
+++ src/app/constants/Skills.ts
@@ -0,0 +1,8 @@
+  // For icons https://icons8.com/icons/set/css
+export const Skills  = [
+      { name: 'JavaScript', image: 'https://img.icons8.com/?size=48&id=PXTY4q2Sq2lG&format=png'},
+      { name: 'Html', image: 'https://img.icons8.com/?size=48&id=20909&format=png' },
+      { name: 'Css', image: 'https://img.icons8.com/?size=60&id=3BTBsJs5myRy&format=png'},
+      { name: 'React.js', image: 'https://img.icons8.com/?size=80&id=wPohyHO_qO1a&format=png'},
+      { name: 'Redux', image: 'https://img.icons8.com/?size=80&id=3VGtaw5gCc8T&format=png'},
+      { name: 'BootStrap', image: 'https://img.icons8.com/?size=48&id=EzPCiQUqWWEa&format=png'},];
